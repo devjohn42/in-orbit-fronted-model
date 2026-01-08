5d4e056c4b7480c3e3d822c38b37b32b604b4aa9
devjohn42 <devjohncb@gmail.com>
2026-01-08 01:38:22 -0300

    :sparkles: feat: create a function to mark the goal as completed

--- src/components/pending-goals.tsx
+++ src/components/pending-goals.tsx
@@ -1,15 +1,25 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
+import { createGoalCompletion } from '../http/create-goal-completion'
 import { getPendingGoals } from '../http/get-pending-goals'
 import { OutlineButton } from './ui/outline-button'
 
 export function PendignGoals() {
+	const queryCliente = useQueryClient()
+
 	const { data } = useQuery({
 		queryKey: ['pending-goals'],
 		queryFn: getPendingGoals
 	})
 
 	if (!data) return null
+
+	async function handleCompleteGoal(goalId: string) {
+		await createGoalCompletion(goalId)
+
+		queryCliente.invalidateQueries({ queryKey: ['summary'] })
+		queryCliente.invalidateQueries({ queryKey: ['pending-goals'] })
+	}
 
 	return (
 		<div className="flex flex-wrap gap-3">
@@ -18,6 +28,7 @@ export function PendignGoals() {
 					<OutlineButton
 						key={goal.id}
 						disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+						onClick={() => handleCompleteGoal(goal.id)}
 					>
 						<Plus className="size-4 text-zinc-600" />
 						{goal.title}
